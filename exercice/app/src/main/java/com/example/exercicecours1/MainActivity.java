79a839188871cd04c46d15bee76ee7b58adf4b76
Mick Bergeron <mickaelbergeron14@gmail.com>
2020-03-13 08:38:36 -0400

    added btn click event and compare two number

--- exercice/app/src/main/java/com/example/exercicecours1/MainActivity.java
+++ exercice/app/src/main/java/com/example/exercicecours1/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.exercicecours1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnClick;
+    private EditText numberOne;
+    private EditText numberTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnClick = findViewById(R.id.button) ;
+        btnClick.setOnClickListener(this);
+
+        numberOne = findViewById(R.id.editText);
+        numberTwo = findViewById(R.id.editText2);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btnClick)
+        {
+            int int1 = Integer.parseInt(numberOne.getText().toString());
+            int int2 = Integer.parseInt(numberTwo.getText().toString());
+
+            if(int1 < int2)
+            {
+                Toast.makeText(this,
+                        "Nombre 2 est plus grand", Toast.LENGTH_LONG).show();
+            }
+            else if (int1 > int2)
+            {
+                Toast.makeText(this,
+                        "Nombre 1 est plus grand", Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                Toast.makeText(this,
+                        "Les nombres sont égaux", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
